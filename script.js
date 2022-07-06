ffb72f2621b2db4a19865a9f7c5c190811c2bc2f
BrunoGaruda <bruleorn@gmail.com>
2022-07-06 18:24:40 -0300

    refatorando o código quando o dado for 1, e corrigindo a lógica do jogo

--- script.js
+++ script.js
@@ -23,6 +23,16 @@ const scores = [0, 0]
 let currentScore = 0
 let activePlayer = 0
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0
+  currentScore = 0
+  activePlayer = activePlayer === 0 ? 1 : 0
+
+  // Toggle irá checar se a classe estará la, caso contrário ele removerá
+  player0El.classList.toggle('player--active')
+  player1El.classList.toggle('player--active')
+}
+
 // Rolling dince funcionalitty
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -40,23 +50,19 @@ btnRoll.addEventListener('click', function () {
       currentScore
   } else {
     // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0
-    currentScore = 0
-    activePlayer = activePlayer === 0 ? 1 : 0
-    // Toggle irá checar se a classe estará la, caso contrário ele removerá
-    player0El.classList.toggle('player--active')
-    player1El.classList.toggle('player--active')
+    switchPlayer()
   }
 })
 
 btnHold.addEventListener('click', function () {
   // 1. Add current score to activePlayer score
   scores[activePlayer] += currentScore
-  document.getElementById(`current--${activePlayer}`).textContent =
+  document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer]
 
   // 2. Check iif player's score is >= 100
   // Finish the Gamepad
 
   // 3. Switch to the next player
+  switchPlayer()
 })
