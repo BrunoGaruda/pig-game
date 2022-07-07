b97daa81ee75b6d64ef414aa524b70966a460b8a
BrunoGaruda <bruleorn@gmail.com>
2022-07-07 16:42:02 -0300

    lógica do jogo pronta, falta concluir o new game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Selectin elements
+// Constantes dos elementos
 const player0El = document.querySelector('.player--0')
 const player1El = document.querySelector('.player--1')
 
@@ -14,7 +14,7 @@ const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
-// Starting conditions
+// Condições
 score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
@@ -22,6 +22,8 @@ diceEl.classList.add('hidden')
 const scores = [0, 0]
 let currentScore = 0
 let activePlayer = 0
+// Criada a variável para finalizar o jogo
+let playing = true
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0
@@ -33,36 +35,51 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active')
 }
 
-// Rolling dince funcionalitty
+// Lógica da rolagem do dado
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1
-  console.log(dice)
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1
+    console.log(dice)
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden')
-  diceEl.src = `assets/dice-${dice}.png` // Hack para alterar o texto
+    // 2. Mostrando o dado na tela
+    diceEl.classList.remove('hidden')
+    diceEl.src = `assets/dice-${dice}.png` // Hack para alterar o texto
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    currentScore += dice
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore
-  } else {
-    // Switch to next player
-    switchPlayer()
+    // 3. Checando caso role 1 no dado
+    if (dice !== 1) {
+      currentScore += dice
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore
+    } else {
+      // Trocar para o próximo jogador
+      switchPlayer()
+    }
   }
 })
 
 btnHold.addEventListener('click', function () {
-  // 1. Add current score to activePlayer score
-  scores[activePlayer] += currentScore
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer]
+  if (playing) {
+    // 1. Adicionando o score atual ao active player sccore
+    scores[activePlayer] += currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer]
 
-  // 2. Check iif player's score is >= 100
-  // Finish the Gamepad
+    // 2. Checar se o score é >= 100
+    if (scores[activePlayer] >= 20) {
+      playing = false
+      // Removendo o dado da tela ao finalizar o game
+      diceEl.classList.add('hidden')
 
-  // 3. Switch to the next player
-  switchPlayer()
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner')
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active')
+    } else {
+      // 3. Trocar para o próximo jogador
+      switchPlayer()
+    }
+  }
 })
